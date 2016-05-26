1136f858f2203b37398fcbd6d3d595d0cfc330c9
Mike James <mike@pebblecode.com>
2016-05-26 11:19:33 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+function leftPad(value, maxLength = 6) {
+  return '0'.repeat(Math.max(0, maxLength - value.length)) + value;
+}
+
+function addSpaces(str, spaceInterval) {
+  let withSpaces = '';
+  for (let i = 0; i < str.length; i++) {
+    if (i % spaceInterval === 0 && i !== 0) {
+      withSpaces += ' ' + str.charAt(i);
+    } else {
+      withSpaces += str.charAt(i);
+    }
+  }
+
+  const spacesStripped = withSpaces.replace(/\s/g, '');
+  if (spacesStripped.length % spaceInterval === 1) {
+    return addSpaces('0' + spacesStripped, spaceInterval);
+  }
+
+  return withSpaces;
+}
+
+function leftPadWithSpace(hexValue, charlength, spaceInterval = 2) {
+  const padded = leftPad(hexValue, charlength);
+  return addSpaces(padded, spaceInterval);
+}
+
+export function intToPrettyHex(int, charlength = 8) {
+
+  const hexValue = int.toString(16);
+
+  return leftPadWithSpace(hexValue, charlength).toUpperCase();
+}
---
+++ test/formatedHexString_spec.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import { intToPrettyHex } from '../src';
+
+describe('hex formatted as XX XX XX XX', () => {
+  it('should format 203', () => {
+    const formatted = intToPrettyHex(203);
+    expect(formatted).to.equal('00 00 00 CB');
+  });
+
+  it('should format 2003', () => {
+    const formatted = intToPrettyHex(2003);
+    expect(formatted).to.equal('00 00 07 D3');
+  });
+
+  it('should format 20003', () => {
+    const formatted = intToPrettyHex(20003);
+    expect(formatted).to.equal('00 00 4E 23');
+  });
+
+  it('should format 200003', () => {
+    const formatted = intToPrettyHex(200003);
+    expect(formatted).to.equal('00 03 0D 43');
+  });
+
+  it('should format 2000003', () => {
+    const formatted = intToPrettyHex(2000003);
+    expect(formatted).to.equal('00 1E 84 83');
+  });
+
+  it('should format 20000003', () => {
+    const formatted = intToPrettyHex(20000003);
+    expect(formatted).to.equal('01 31 2D 03');
+  });
+
+  it('should format 200000003', () => {
+    const formatted = intToPrettyHex(200000003);
+    expect(formatted).to.equal('0B EB C2 03');
+  });
+
+  it('should format 2000000003', () => {
+    const formatted = intToPrettyHex(2000000003);
+    expect(formatted).to.equal('77 35 94 03');
+  });
+
+  it('should format 20000000003', () => {
+    const formatted = intToPrettyHex(20000000003);
+    expect(formatted).to.equal('04 A8 17 C8 03');
+  });
+});
